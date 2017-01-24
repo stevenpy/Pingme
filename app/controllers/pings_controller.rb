8b1f645fed22a68f242746c36c4d4cd59fd296e0
stevenpy <stevenphilbert@gmail.com>
2017-01-24 15:16:55 +0100

    coding style review

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -1,63 +1,63 @@
 class PingsController < ApplicationController
-	before_action :set_ping, only: [:show, :edit, :update, :destroy]
-	before_action :owned_post, only: [:edit, :update, :destroy]
-	before_action :authenticate_user!
+  before_action :set_ping, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
-	def index
-		@pings = Ping.all
-	end
+  def index
+    @pings = Ping.all
+  end
 
-	def new
-		@ping = current_user.pings.build
-	end
+  def new
+    @ping = current_user.pings.build
+  end
 
-	def create
-		@ping = current_user.pings.build(ping_params)
+  def create
+    @ping = current_user.pings.build(ping_params)
 
-		if @ping.save
-			flash[:success] = "Your ping has been created!"
-			redirect_to pings_path
-		else
-			flash.now[:alert] = "Your new ping couldn't be created!"
-			render :new
-		end
-	end
+    if @ping.save
+      flash[:success] = "Your ping has been created!"
+      redirect_to pings_path
+    else
+      flash.now[:alert] = "Your new ping couldn't be created!"
+      render :new
+    end
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @ping.update(ping_params)
-			flash[:success] = "Ping updated."
-			redirect_to pings_path
-		else
-			flash.now[:alert] = "Update failed.  Please check the form."
-			render :edit
-		end
-	end
+  def update
+    if @ping.update(ping_params)
+      flash[:success] = "Ping updated."
+      redirect_to pings_path
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+  end
 
-	def destroy
-		@ping.destroy
-		redirect_to root_path
-	end
+  def destroy
+    @ping.destroy
+    redirect_to root_path
+  end
 
-	private
+  private
 
-	def ping_params
-		params.require(:ping).permit(:image, :caption)
-	end
+  def ping_params
+    params.require(:ping).permit(:image, :caption)
+  end
 
-	def set_ping
+  def set_ping
     @ping = Ping.find(params[:id])
   end
 
   def owned_post
-  	unless current_user == @ping.user
-    	flash[:alert] = "That ping doesn't belong to you!"
-    	redirect_to root_path
-  	end
-	end
+    unless current_user == @ping.user
+      flash[:alert] = "That ping doesn't belong to you!"
+      redirect_to root_path
+    end
+  end
 end
