720bb94a17573390e51d710fe39b281244158b89
stevenpy <stevenphilbert@gmail.com>
2017-01-27 15:56:27 +0100

    Can click on username in comments

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,16 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :owned_profile, only: [:edit, :update]
+  before_action :set_user
 
   def show
-    @user = User.find_by(user_name: params[:user_name])
-    @pings = User.find_by(user_name: params[:user_name]).pings.order('created_at DESC')
+    @pings = @user.pings.order('created_at DESC')
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
   end
 
   def update
-    @user = User.find_by(user_name: params[:user_name])
     if @user.update(profile_params)
       flash[:success] = 'Your profile has been updated.'
       redirect_to profile_path(@user.user_name)
@@ -30,10 +28,13 @@ class ProfilesController < ApplicationController
   end
 
   def owned_profile
-    @user = User.find_by(user_name: params[:user_name])
     unless current_user == @user
       flash[:alert] = "That profile doesn't belong to you!"
       redirect_to root_path
     end
+  end
+
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
   end
 end
